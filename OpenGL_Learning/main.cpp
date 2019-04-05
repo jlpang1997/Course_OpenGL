77621040854bc060c38b18852ea875b11ed1cab8
jlpang1997 <JLPANG1997@outlook.com>
2019-04-05 12:55:06 +0800

    基本图元的生成

--- OpenGL_Learning/main.cpp
+++ OpenGL_Learning/main.cpp
@@ -21,6 +21,50 @@ void point_display()
 	glEnd();
 	glFlush();
 }
+void line_display()
+{
+	//GL_LINES 两个点配对，所以必须有偶数个点
+	//GL_LINE_STRIP 首尾相连但不闭合
+	//GL_LINE_LOOP 首尾相连而且闭合
+	glLineWidth(3);//设定宽度
+
+	glEnable(GL_LINE_STIPPLE);//开启虚线模式
+
+	glLineStipple(3, 0b1001001001001000);//设置虚线属性
+	glBegin(GL_LINE_LOOP);
+	{
+		glVertex2f(-0.8, -0.5);
+		glVertex2f(-0.5, -0.5);
+		glVertex2f(-0.8, 0);
+		glVertex2f(-0.5, 0);
+		glVertex2f(-0.8, 0.5);
+		glVertex2f(-0.3, 0.5);
+	}
+	glEnd();
+	glFlush();
+}
+void polygon_display()//opengl只能画凸多边形
+{
+	glPointSize(10);
+	glFrontFace(GL_CCW);//逆时针为正面
+	glFrontFace(GL_CW);//顺时针
+	//glBegin(GL_POLYGON);
+	//glBegin(GL_TRIANGLES);
+	//glBegin(GL_TRIANGLE_STRIP);
+	glPolygonMode(GL_FRONT, GL_POINT);//GL_POINT是用来设置模式的，而GL_POINTS是用来画点的
+	glPolygonMode(GL_BACK, GL_FILL);//这说明当我们画二维的时候，看到的是背面
+	glBegin(GL_QUADS);//四边形
+	{
+		glVertex2f(0, -0.1);
+		glVertex2f(-0.5, 0);
+		glVertex2f(0, 0.5);
+		glVertex2f(0.5, 0);
+		glVertex2f(0, -0.5);
+
+	}
+	glEnd();
+	glFlush();
+}
 int main(int argc, char **argv)
 {
 	glutInit(&argc, argv);
@@ -29,7 +73,9 @@ int main(int argc, char **argv)
 	glutInitWindowSize(500, 500);
 	glutCreateWindow("helle world");
 
-	glutDisplayFunc(point_display);
+	//glutDisplayFunc(point_display);
+	//glutDisplayFunc(line_display);
+	glutDisplayFunc(polygon_display);
 
 	glutMainLoop();
 	return 0;
